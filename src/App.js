b9187a392c8eeb91503f0dcda0e26bb070e79252
sstrawmatt2 <50216928+sstrawmatt2@users.noreply.github.com>
2020-10-01 16:58:03 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Users from './components/users';
 
-function App() {
+class App extends Component {
+    state ={
+        users=[]
+
+    }
+
+    componentDidMount(){
+
+        fetch("")
+        .then(res => res.json())
+        .then((data) => {
+            this.setState({ users: data })
+
+        })
+        .catch(console.log)
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Users users = {this.state.users} />
   );
 }
 
